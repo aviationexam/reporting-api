881b8c3140653634001fe80bead631957295e45b
Jan Trejbal <jan@trejbal.land>
2023-11-15 18:26:47 +0100

    Add ReportingApiJsonSerializerContext (#24)
    * Add ReportingApiJsonSerializerContext
    
    * Update JsonSourceGenerationOptions

---
+++ src/ReportingApi/ReportingApiJsonSerializerContext.cs
@@ -0,0 +1,12 @@
+﻿using ReportingApi.Models;
+using System.Text.Json.Serialization;
+
+namespace ReportingApi;
+
+[JsonSourceGenerationOptions(
+    GenerationMode = JsonSourceGenerationMode.Default
+)]
+[JsonSerializable(typeof(ReportRequest))]
+[JsonSerializable(typeof(ReportRequest<CspReport>))]
+[JsonSerializable(typeof(ReportRequest<NetworkErrorReport>))]
+public sealed partial class ReportingApiJsonSerializerContext : JsonSerializerContext;
