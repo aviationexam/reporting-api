e722c7d93c42bbfee6c27c7e7962b5f2e778968f
Jan Trejbal <jan@trejbal.land>
2023-11-16 14:09:01 +0100

    Add IReadOnlyCollection<ReportRequest> into ReportingApiJsonSerializerContext

--- src/ReportingApi/ReportingApiJsonSerializerContext.cs
+++ src/ReportingApi/ReportingApiJsonSerializerContext.cs
@@ -1,4 +1,5 @@
 using ReportingApi.Models;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace ReportingApi;
@@ -9,4 +10,5 @@ namespace ReportingApi;
 [JsonSerializable(typeof(ReportRequest))]
 [JsonSerializable(typeof(ReportRequest<CspReport>))]
 [JsonSerializable(typeof(ReportRequest<NetworkErrorReport>))]
+[JsonSerializable(typeof(IReadOnlyCollection<ReportRequest>))]
 public sealed partial class ReportingApiJsonSerializerContext : JsonSerializerContext;
